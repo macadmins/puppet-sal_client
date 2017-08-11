56a39c17648d596d0884c31be5bd11df8dd43a1e
brett_demetris <brett.demetris@airbnb.com>
2017-08-11 14:48:29 -0700

    deploys gosal to windows machines

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,26 @@
 class sal_client (
   $server,
   $key,
-  $version = '5f7193e22831d83c4bb0be0e52b3960019f8a7ee',
-  $skip_facts = [],
-  $sync_scripts = true,
-  $basic_auth = true,
-  $payload_organization = 'Sal Opensource',
+  $source,
+  $version,
+  $gosal_version,
+  $skip_facts,
+  $sync_scripts,
+  $basic_auth,
+  $payload_organization,
 ){
-  if $facts['os']['name'] == 'Darwin' {
-    include xcode_tools
-    class {'sal_client::install':
-      require => Class['xcode_tools'],
-    } ->
-    class {'sal_client::config': } ->
-    Class['sal_client']
+  case $facts['os']['name'] {
+    'Darwin': {
+      include xcode_tools
+      class {'sal_client::install':
+        require => Class['xcode_tools'],
+      } ->
+      class {'sal_client::config': } ->
+      Class['sal_client']
+      }
+    'Windows': {
+      class {'sal_client::windows_install': } ->
+      Class['sal_client']
+    }
   }
 }
